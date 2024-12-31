f641863bcfe11f06128c65cf5bfc09fa94955168
cuong <tranhungcuong31720@gmail.com>
2024-12-31 13:18:02 +0700

    update login

--- my_flask_project/venv/app/run.py
+++ my_flask_project/venv/app/run.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, jsonify, abort, render_template, redirect
-import oracledb
+from flask import Flask, request, jsonify, abort, render_template, redirect, flash, session, make_response
+import oracledb, datetime
 from flask_cors import CORS
-import random
+import jwt
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'abc'
 # Thông tin kết nối đến Oracle Database
 DB_CONFIG = {
     "username": "system",
@@ -25,7 +25,97 @@ def get_db_connection():
         abort(500, description="Không thể kết nối cơ sở dữ liệu.")
 @app.route('/configForm', methods=['GET'])
 def show_form():
-    return render_template('index.html') 
+    token = request.cookies.get('token')
+    if not token:
+        return redirect(('login'))
+    try:
+        data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        username = data['username']
+    except jwt.ExpiredSignatureError:
+        flash('Token has expired, please log in again.', 'error')
+        return redirect(('login'))
+    except jwt.InvalidTokenError:
+        flash('Invalid token, please log in again.', 'error')
+        return redirect(('login'))
+    return render_template('index.html', username=username)
+@app.route('/logout', methods=['GET'])
+def logout():
+    resp = make_response(redirect(('login')))
+    resp.delete_cookie('token')
+    return resp
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username').strip()
+        password = request.form.get('password').strip()
+        # Kiểm tra tài khoản admin
+        if username == 'admin' and password == '123':
+            return redirect('/configForm')
+        # Kết nối tới cơ sở dữ liệu
+        try:
+            connection = get_db_connection()
+            cursor = connection.cursor()
+            query = """
+                SELECT COUNT(*)
+                FROM USERS
+                WHERE USERNAME = :username AND PASSWORD = :password
+            """
+            cursor.execute(query, {"username": username, "password": password})
+            rows = cursor.fetchone()
+            # Kiểm tra nếu tài khoản tồn tại
+            if rows and rows[0] > 0:
+                token = jwt.encode({
+                    'username': username,
+                    'exp': datetime.datetime.utcnow() + datetime.timedelta(days=7)  # Token có thời hạn 7 ngày
+                }, app.config['SECRET_KEY'], algorithm='HS256')
+                resp = make_response(redirect(('configForm')))
+                resp.set_cookie('token', token, httponly=True, max_age=7*24*60*60)  # Cookie có thời hạn 7 ngày
+                return resp
+            else:
+                return render_template('login.html', error='Incorrect username or password')
+        except Exception as e:
+            flash(f"Database error: {e}", "error")
+        finally:
+            if cursor:
+                cursor.close()
+            if connection:
+                connection.close()
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # Xử lý dữ liệu đăng ký
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # Thêm logic để lưu thông tin người dùng vào cơ sở dữ liệu
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        query = """
+            SELECT USERNAME
+            FROM USERS
+            WHERE USERNAME = :username 
+        """
+        cursor.execute(query, {"username": username})
+        rows = cursor.fetchone()
+        if rows:
+            return render_template('register.html', error='User already exists')
+        else:
+            insert_query = """
+                INSERT INTO USERS (USERNAME, PASSWORD)
+                VALUES (:username, :password)
+            """
+            cursor.execute(insert_query, {"username": username, "password": password})
+            connection.commit()
+            cursor.close()
+            connection.close()
+            return render_template('login.html', error='')
+    return render_template('register.html')
+
+
+
 # API: Lấy danh sách thông tin bảng CNT_MACHINE_SUMMARY
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
@@ -34,7 +124,7 @@ def get_tasks():
     cursor = connection.cursor()
 
     # Lấy tham số phân trang từ request
-    page = int(request.args.get('page', 1))  # Trang hiện tại (mặc định = 1)
+    page = int(request.args.get('page', 1))
     per_page = int(request.args.get('per_page', 5))  # Số bản ghi mỗi trang (mặc định = 5)
 
     # Tính toán offset
@@ -187,10 +277,9 @@ def submitConfig():
         })
         connection.commit()
         cursor.close()
-
         return render_template('/result.html')
-
     except Exception as e:
+        
         return jsonify({"error": str(e)}), 500
 
 @app.route('/returnHome', methods=['GET'])
@@ -247,7 +336,6 @@ def chart_data2():
     # Kết nối tới cơ sở dữ liệu
     connection = get_db_connection()
     cursor = connection.cursor()
-    
     # Câu lệnh SQL lấy dữ liệu từ bảng CNT_MACHINE_SUMMARY
     sql = """
         SELECT WORK_DATE, RUN_TIME, ERROR_TIME
@@ -259,8 +347,8 @@ def chart_data2():
     
     # Tạo danh sách labels và data
     labels = [str(row[0]) for row in result] 
-    data1 = [row[1] for row in result]
-    data2 = [row[2] for row in result] 
+    runTime = [row[1] for row in result]
+    errorTime = [row[2] for row in result] 
 
     # Cấu trúc dữ liệu trả về
     data = {
@@ -268,13 +356,13 @@ def chart_data2():
         "datasets": [
             {
                 "label": "RUN TIME",
-                "data": data1,
+                "data": runTime,
                 "borderColor": "rgb(54, 162, 235)",
                 "fill": False
             },
             {
                 "label": "ERROR TIME",
-                "data": data2,
+                "data": errorTime,
                 "borderColor": "rgb(255, 99, 132)",
                 "fill": False
             }
